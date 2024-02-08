defbd1f2618ee38544fb753ac5ddab21b01295d0
Alexander Alemayhu <alexander@alemayhu.com>
2024-02-08 21:36:56 +0100

    play with maps

--- lib/koans/08_maps.ex
+++ lib/koans/08_maps.ex
@@ -34,24 +34,24 @@ defmodule Maps do
 
   koan "Or you can use some syntactic sugar for existing elements" do
     younger_person = %{@person | age: 16}
-    assert Map.fetch(younger_person, :age) == ___
+    assert Map.fetch(younger_person, :age) == {:ok, 16}
   end
 
   koan "Can remove pairs by key" do
     without_age = Map.delete(@person, :age)
-    assert Map.has_key?(without_age, :age) == ___
+    assert Map.has_key?(without_age, :age) == false
   end
 
   koan "Can merge maps" do
-    assert Map.merge(%{first_name: "Jon"}, %{last_name: "Snow"}) == ___
+    assert Map.merge(%{first_name: "Jon"}, %{last_name: "Snow"}) == %{first_name: "Jon", last_name: "Snow"}
   end
 
   koan "When merging, the last map wins" do
     merged = Map.merge(@person, %{last_name: "Baratheon"})
-    assert Map.fetch(merged, :last_name) == ___
+    assert Map.fetch(merged, :last_name) == {:ok, "Baratheon"}
   end
 
   koan "You can also select sub-maps out of a larger map" do
-    assert Map.take(@person, [:first_name, :last_name]) == ___
+    assert Map.take(@person, [:first_name, :last_name]) == %{first_name: "Jon", last_name: "Snow"}
   end
 end
